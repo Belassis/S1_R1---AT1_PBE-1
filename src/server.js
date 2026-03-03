62fb229e81bd7aa9b6e3267fc9535a37b2126b46
Isinha <isabelly.assis.5261@gmail.com>
2026-03-03 10:54:45 -0300

    atvd

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ import path from 'path';
 import 'dotenv/config';
 
 const app = express();
+
+app.use(express.json());
 app.use('/', imagemRoutes);
 
 app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Servidor rodando em http://localhost${process.env.SERVER_PORT}`); 
+    console.log(`Servidor rodando em http://localhost:${process.env.SERVER_PORT}`); 
 })
